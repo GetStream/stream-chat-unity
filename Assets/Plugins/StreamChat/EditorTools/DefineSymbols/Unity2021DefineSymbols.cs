970550ca4b4b0549c9a3db96ec6320fb894865db
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2022-06-15 17:19:59 +0200

    Adjusted scripts to Unity 2021.1

--- Assets/Plugins/StreamChat/EditorTools/DefineSymbols/Unity2021DefineSymbols.cs
+++ Assets/Plugins/StreamChat/EditorTools/DefineSymbols/Unity2021DefineSymbols.cs
@@ -9,22 +9,37 @@ namespace StreamChat.EditorTools.DefineSymbols
     {
         public string[] GetScriptingDefineSymbols(BuildTarget buildTarget)
         {
+#if UNITY_2021_1
+            var group = BuildPipeline.GetBuildTargetGroup(buildTarget);
+            var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
+
+            return symbols.Split(';');
+#else
             var group = BuildPipeline.GetBuildTargetGroup(buildTarget);
             var namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(group);
 
             var symbols = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget);
 
             return symbols.Split(';');
+#endif
+
+
         }
 
         public void SetScriptingDefineSymbols(BuildTarget buildTarget, ICollection<string> defines)
         {
+#if UNITY_2021_1
+            var group = BuildPipeline.GetBuildTargetGroup(buildTarget);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", defines));
+#else
+
             var group = BuildPipeline.GetBuildTargetGroup(buildTarget);
             var namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(group);
 
             var combinedSymbols = string.Join(";", defines);
 
             PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, combinedSymbols);
+#endif
         }
     }
 }
