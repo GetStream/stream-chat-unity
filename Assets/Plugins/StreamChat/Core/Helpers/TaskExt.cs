6965adabb02178024bd36efc9a1e15b3734bccb9
Daniel Sierpiński <sierpinski.d@gmail.com>
2022-09-07 20:35:18 +0200

    Add utility method to allow running Task as callback (#44)

---
+++ Assets/Plugins/StreamChat/Core/Helpers/TaskExt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StreamChat.Core.Helpers
+{
+    /// <summary>
+    /// Extensions for Task
+    /// </summary>
+    public static class TaskExt
+    {
+        /// <summary>
+        /// Run task as callback
+        /// </summary>
+        /// <param name="onSuccess">Called when task succeeds. Contains response object</param>
+        /// <param name="onFailure">Called when task fails. Contains thrown exception.</param>
+        /// <typeparam name="TResponse">Type of response when task succeeds.</typeparam>
+        public static void AsCallback<TResponse>(this Task<TResponse> task, Action<TResponse> onSuccess = null, Action<Exception> onFailure = null)
+        {
+            task.ContinueWith(_ =>
+            {
+                if (_.IsFaulted)
+                {
+                    onFailure?.Invoke(_.Exception);
+                    return;
+                }
+
+                onSuccess?.Invoke(_.Result);
+            });
+        }
+    }
+}
