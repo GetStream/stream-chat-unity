6c0c2f28341cc51d9f6872071fff4d629e727e6c
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2022-11-01 16:37:58 +0100

    Add support for devices create/list/remove endpoints

---
+++ Assets/Plugins/StreamChat/Core/API/IDeviceApi.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using StreamChat.Core.Requests;
+using StreamChat.Core.Responses;
+
+namespace StreamChat.Core.API
+{
+    internal interface IDeviceApi
+    {
+        Task<ApiResponse> AddDeviceAsync(CreateDeviceRequest device);
+
+        Task<ListDevicesResponse> ListDevicesAsync(string userId);
+
+        Task<ApiResponse> RemoveDeviceAsync(string deviceId, string userId);
+    }
+}
