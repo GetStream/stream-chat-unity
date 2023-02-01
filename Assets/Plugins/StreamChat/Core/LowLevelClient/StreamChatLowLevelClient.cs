20a1dff04f3e2f8036b8f221200191c8717c0ecb
Daniel Sierpiński <sierpinski.d@gmail.com>
2023-02-01 08:45:55 +0100

    Change connection update monitor to not throw error if client is disposed or closing (#89)
    * Change connection update monitor to not throw error if client is disposed or closing + fix test that threw this error due to client not being ever disposed
    
    * Add disposing additional resources in the TearDown in other to dispose additional clients before they throw "connection not updated" error
    
    * Add an initial update to prevent "connection is not updated" error during the low level client unit tests
    
    * Disable update monitor when tests are enabled

--- Assets/Plugins/StreamChat/Core/LowLevelClient/StreamChatLowLevelClient.cs
+++ Assets/Plugins/StreamChat/Core/LowLevelClient/StreamChatLowLevelClient.cs
@@ -338,7 +338,9 @@ namespace StreamChat.Core.LowLevelClient
 
         public void Update(float deltaTime)
         {
+#if !STREAM_TESTS_ENABLED
             _updateCallReceived = true;
+#endif
 
             TryHandleWebsocketsConnectionFailed();
             TryToReconnect();
@@ -402,7 +404,9 @@ namespace StreamChat.Core.LowLevelClient
 
             _websocketClient.ConnectionFailed -= OnWebsocketsConnectionFailed;
             _websocketClient.Disconnected -= OnWebsocketDisconnected;
-            _websocketClient?.Dispose();
+            _websocketClient.Dispose();
+            
+            _updateMonitorCts.Cancel();
         }
 
         string IAuthProvider.ApiKey => _authCredentials.ApiKey;
@@ -486,6 +490,7 @@ namespace StreamChat.Core.LowLevelClient
         private TaskCompletionSource<OwnUserInternalDTO> _connectUserTaskSource;
         private CancellationToken _connectUserCancellationToken;
         private CancellationTokenSource _connectUserCancellationTokenSource;
+        private CancellationTokenSource _updateMonitorCts;
 
         private AuthCredentials _authCredentials;
 
@@ -952,15 +957,20 @@ namespace StreamChat.Core.LowLevelClient
 
         private void LogErrorIfUpdateIsNotBeingCalled()
         {
-            const int Timeout = 2;
-            Task.Delay(Timeout * 1000).ContinueWith(t =>
+            _updateMonitorCts = new CancellationTokenSource();
+            
+            //StreamTodo: temporarily disable update monitor when tests are enabled -> investigate why some tests trigger this error
+#if !STREAM_TESTS_ENABLED
+            const int timeout = 2;
+            Task.Delay(timeout * 1000, _updateMonitorCts.Token).ContinueWith(t =>
             {
-                if (!_updateCallReceived && ConnectionState != ConnectionState.Disconnected)
+                if (!_updateCallReceived && !_updateMonitorCts.IsCancellationRequested && ConnectionState != ConnectionState.Closing)
                 {
                     _logs.Error(
-                        $"Connection is not being updated. Please call the `{nameof(StreamChatLowLevelClient)}.{nameof(Update)}` method per frame.");
+                        $"Connection is not being updated. Please call the `{nameof(StreamChatLowLevelClient)}.{nameof(Update)}` method per frame. Connection state: {ConnectionState}");
                 }
-            });
+            }, _updateMonitorCts.Token);
+#endif
         }
 
         private static string BuildStreamClientHeader(IApplicationInfo applicationInfo)
