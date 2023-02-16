910f520b7e8d210f9153c1f9e2fe9b15eb1ad214
Daniel Sierpiński <sierpinski.d@gmail.com>
2023-02-16 09:21:57 +0100

    Feature/refactor tests runner to reduce number of connections (#107)
    * Implement IStreamChannel.MembersChanged event to trigger on add, remove, update + add integration tests
    
    * Wrap tests file in a compiler flag
    
    * Fix WaitWhileConditionTrue and WaitWhileConditionFalse early exit
    
    * Increase waiting time for data propagation
    
    * Refactor tests runner to maintain a single client throughout all tests

---
+++ Assets/Plugins/StreamChat/Tests/UnityTestRunnerCallbacks.cs
@@ -0,0 +1,37 @@
+﻿#if STREAM_TESTS_ENABLED
+using System;
+using NUnit.Framework.Interfaces;
+using StreamChat.Tests;
+using UnityEngine.TestRunner;
+
+[assembly: TestRunCallback(typeof(UnityTestRunnerCallbacks))]
+
+namespace StreamChat.Tests
+{
+    /// <summary>
+    /// Receives callback from Unity Editor
+    /// </summary>
+    internal class UnityTestRunnerCallbacks : ITestRunCallback
+    {
+        public static UnityTestRunnerCallbacks Instance { get; private set;}
+
+        public static event Action<ITest> RunStartedCallback;
+        public static event Action<ITestResult> RunFinishedCallback;
+        public static event Action<ITest> TestStartedCallback;
+        public static event Action<ITestResult> TestFinishedCallback;
+        
+        public UnityTestRunnerCallbacks()
+        {
+            Instance = this;
+        }
+
+        public void RunStarted(ITest testsToRun) => RunStartedCallback?.Invoke(testsToRun);
+
+        public void RunFinished(ITestResult testResults) => RunFinishedCallback?.Invoke(testResults);
+
+        public void TestStarted(ITest test) => TestStartedCallback?.Invoke(test);
+
+        public void TestFinished(ITestResult result) => TestFinishedCallback?.Invoke(result);
+    }
+}
+#endif
