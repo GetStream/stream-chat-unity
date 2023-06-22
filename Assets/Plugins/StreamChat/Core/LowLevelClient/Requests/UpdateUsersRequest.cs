420b271f077d1948cc46193150aa2003a1a7494f
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2023-06-22 15:49:31 +0200

    Experimental fix for Ticket(38178) - TrySaveToDtoDictionary crash on Android(7, 10, 11, 12) & IL2CPP

--- Assets/Plugins/StreamChat/Core/LowLevelClient/Requests/UpdateUsersRequest.cs
+++ Assets/Plugins/StreamChat/Core/LowLevelClient/Requests/UpdateUsersRequest.cs
@@ -1,4 +1,4 @@
-﻿using StreamChat.Core.Helpers;
+﻿using System.Collections.Generic;
 using StreamChat.Core.InternalDTO.Requests;
 
 namespace StreamChat.Core.LowLevelClient.Requests
@@ -11,11 +11,33 @@ namespace StreamChat.Core.LowLevelClient.Requests
         /// StreamTodo: Check if this could be list
         public System.Collections.Generic.Dictionary<string, UserObjectRequest> Users { get; set; }
 
-        UpdateUsersRequestInternalDTO ISavableTo<UpdateUsersRequestInternalDTO>.SaveToDto() =>
-            new UpdateUsersRequestInternalDTO
+        UpdateUsersRequestInternalDTO ISavableTo<UpdateUsersRequestInternalDTO>.SaveToDto()
+        {
+            var dto = new UpdateUsersRequestInternalDTO
             {
-                Users = Users.TrySaveToDtoDictionary<UserObjectRequestInternalDTO, UserObjectRequest, string>(),
                 AdditionalProperties = AdditionalProperties,
             };
+
+            // Ticket#38178 TrySaveToDtoDictionary caused crashes on old Android versions with IL2CPP
+            if (Users != null)
+            {
+                var dict = new Dictionary<string, UserObjectRequestInternalDTO>();
+            
+                foreach (var sourceKeyValue in Users)
+                {
+                    if (sourceKeyValue.Value == null)
+                    {
+                        continue;
+                    }
+                    
+                    dict.Add(sourceKeyValue.Key,
+                        ((ISavableTo<UserObjectRequestInternalDTO>)sourceKeyValue.Value).SaveToDto());
+                }
+            
+                dto.Users = dict;
+            }
+
+            return dto;
+        }
     }
 }
