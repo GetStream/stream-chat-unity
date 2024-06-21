4b11141b82a9c6777a351dae599c7d0e47d156ce
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2024-06-21 12:40:33 +0200

    Add 1 more test case to sync tests

--- Assets/Plugins/StreamChat/Tests/StatefulClient/ClientStateSyncTests.cs
+++ Assets/Plugins/StreamChat/Tests/StatefulClient/ClientStateSyncTests.cs
@@ -1,5 +1,7 @@
 ﻿#if STREAM_TESTS_ENABLED
 using NUnit.Framework;
+using StreamChat.Core.StatefulModels;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -135,6 +137,69 @@ namespace StreamChat.Tests.StatefulClient
         }
 
         //StreamTodo: validate that appropriate events are being triggered on the StreamChatClient instance
+
+
+        [UnityTest]
+        public IEnumerator When_client_sends_message_right_after_reconnect_expect_received_older_messages_to_be_in_correct_order()
+    => ConnectAndExecute(When_client_sends_message_right_after_reconnect_expect_received_older_messages_to_be_in_correct_order_Async);
+
+        private async Task When_client_sends_message_right_after_reconnect_expect_received_older_messages_to_be_in_correct_order_Async()
+        {
+            // Create channel
+            var channel = await CreateUniqueTempChannelAsync();
+
+            var otherClient = await GetConnectedOtherClientAsync();
+
+            // Fetch channel on other client to get it loaded into state layer
+            var otherClientChannel = await otherClient.GetOrCreateChannelWithIdAsync(channel.Type, channel.Id);
+
+            Assert.AreEqual(channel.Cid, otherClientChannel.Cid);
+
+            var otherClientMessage = await otherClientChannel.SendNewMessageAsync("BEFORE DISCONNECT #1");
+
+            // DISCONNECT
+            await otherClient.DisconnectUserAsync();
+            Assert.IsFalse(otherClient.IsConnected);
+
+            // Send messages on the first client
+            var message = await channel.SendNewMessageAsync("RECONNECTED #1");
+            var message2 = await channel.SendNewMessageAsync("RECONNECTED #2");
+
+            // Messages should be presents on the connected client
+            Assert.IsTrue(channel.Messages.Contains(message));
+            Assert.IsTrue(channel.Messages.Contains(message2));
+
+            // And should not be present on the disconnected client
+            Assert.IsFalse(otherClientChannel.Messages.Any(m => m.Id == message.Id));
+            Assert.IsFalse(otherClientChannel.Messages.Any(m => m.Id == message2.Id));
+
+            // Reconnect other client
+            await GetConnectedOtherClientAsync();
+
+            // We do not wait for the /sync to complete
+            var otherClientMessageAfterReconnect = await otherClientChannel.SendNewMessageAsync("AFTER #1");
+            var otherClientMessageAfterReconnect2 = await otherClientChannel.SendNewMessageAsync("AFTER #2");
+
+            // Wait for sync request to complete
+            await WaitWhileTrueAsync(() => otherClientChannel.Messages.All(m => m.Id != message2.Id));
+
+            // Assert correct number of messages
+            Assert.AreEqual(5, otherClientChannel.Messages.Count);
+
+            // Assert no duplicates
+            var uniqueIds = new HashSet<string>();
+            Assert.IsTrue(otherClientChannel.Messages.All(m => uniqueIds.Add(m.Id)));
+
+            var messages = otherClientChannel.Messages.ToArray();
+            // Assert correct order
+            Assert.AreEqual(0, Array.FindIndex(messages, m => m.Id == otherClientMessage.Id));
+            Assert.AreEqual(1, Array.FindIndex(messages, m => m.Id == message.Id));
+            Assert.AreEqual(2, Array.FindIndex(messages, m => m.Id == message2.Id));
+            Assert.AreEqual(3, Array.FindIndex(messages, m => m.Id == otherClientMessageAfterReconnect.Id));
+            Assert.AreEqual(4, Array.FindIndex(messages, m => m.Id == otherClientMessageAfterReconnect2.Id));
+
+
+        }
     }
 }
 #endif
