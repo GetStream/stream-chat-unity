e103ae318315a15c07aa2cf51209770047d5edff
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2024-06-27 14:25:00 +0200

    Upgrade the package to version 4.7.0

--- Assets/Plugins/StreamChat/Core/LowLevelClient/StreamChatLowLevelClient.cs
+++ Assets/Plugins/StreamChat/Core/LowLevelClient/StreamChatLowLevelClient.cs
@@ -205,7 +205,7 @@ namespace StreamChat.Core.LowLevelClient
         /// <summary>
         /// SDK Version number
         /// </summary>
-        public static readonly Version SDKVersion = new Version(4, 6, 0);
+        public static readonly Version SDKVersion = new Version(4, 7, 0);
 
         /// <summary>
         /// Use this method to create the main client instance or use StreamChatClient constructor to create a client instance with custom dependencies
