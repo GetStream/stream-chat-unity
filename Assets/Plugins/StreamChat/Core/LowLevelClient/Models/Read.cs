5f21bcf27098d69c21e3ce74071368cef071fdf7
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2024-08-23 11:09:22 +0200

    Add missing field + make read only

--- Assets/Plugins/StreamChat/Core/LowLevelClient/Models/Read.cs
+++ Assets/Plugins/StreamChat/Core/LowLevelClient/Models/Read.cs
@@ -5,15 +5,18 @@ namespace StreamChat.Core.LowLevelClient.Models
 {
     public class Read : ModelBase, ILoadableFrom<ReadInternalDTO, Read>
     {
-        public System.DateTimeOffset? LastRead { get; set; }
+        public System.DateTimeOffset? LastRead { get; private set; }
+        
+        public string LastReadMessageId { get; private set; }
 
-        public int? UnreadMessages { get; set; }
+        public int? UnreadMessages { get; private set; }
 
-        public User User { get; set; }
+        public User User { get; private set; }
 
         Read ILoadableFrom<ReadInternalDTO, Read>.LoadFromDto(ReadInternalDTO dto)
         {
             LastRead = dto.LastRead;
+            LastReadMessageId = dto.LastReadMessageId;
             UnreadMessages = dto.UnreadMessages;
             User = User.TryLoadFromDto<UserObjectInternalDTO, User>(dto.User);
             AdditionalProperties = dto.AdditionalProperties;
