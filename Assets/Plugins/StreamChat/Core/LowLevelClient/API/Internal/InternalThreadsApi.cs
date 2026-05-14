1acdf2fe82eee869deeedbbb4c33e206ba21db1c
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2026-05-14 11:04:53 +0200

    Fix serializing filter values to GET params

--- Assets/Plugins/StreamChat/Core/LowLevelClient/API/Internal/InternalThreadsApi.cs
+++ Assets/Plugins/StreamChat/Core/LowLevelClient/API/Internal/InternalThreadsApi.cs
@@ -52,7 +52,61 @@ namespace StreamChat.Core.LowLevelClient.API.Internal
 
         public Task<GetRepliesResponseInternalDTO> GetRepliesAsync(string parentId,
             MessagePaginationParamsRequestInternalDTO pagination)
-            => Get<MessagePaginationParamsRequestInternalDTO, GetRepliesResponseInternalDTO>(
-                $"messages/{parentId}/replies", pagination);
+        {
+            var parameters = QueryParameters.Default;
+            if (pagination != null)
+            {
+                if (pagination.Limit.HasValue)
+                {
+                    parameters.Append("limit", pagination.Limit.Value.ToString());
+                }
+                if (pagination.Offset.HasValue)
+                {
+                    parameters.Append("offset", pagination.Offset.Value.ToString());
+                }
+                if (!string.IsNullOrEmpty(pagination.IdGt))
+                {
+                    parameters.Append("id_gt", pagination.IdGt);
+                }
+                if (!string.IsNullOrEmpty(pagination.IdGte))
+                {
+                    parameters.Append("id_gte", pagination.IdGte);
+                }
+                if (!string.IsNullOrEmpty(pagination.IdLt))
+                {
+                    parameters.Append("id_lt", pagination.IdLt);
+                }
+                if (!string.IsNullOrEmpty(pagination.IdLte))
+                {
+                    parameters.Append("id_lte", pagination.IdLte);
+                }
+                if (!string.IsNullOrEmpty(pagination.IdAround))
+                {
+                    parameters.Append("id_around", pagination.IdAround);
+                }
+                if (pagination.CreatedAtAfter.HasValue)
+                {
+                    parameters.Append("created_at_after", pagination.CreatedAtAfter.Value.ToString("o"));
+                }
+                if (pagination.CreatedAtAfterOrEqual.HasValue)
+                {
+                    parameters.Append("created_at_after_or_equal", pagination.CreatedAtAfterOrEqual.Value.ToString("o"));
+                }
+                if (pagination.CreatedAtBefore.HasValue)
+                {
+                    parameters.Append("created_at_before", pagination.CreatedAtBefore.Value.ToString("o"));
+                }
+                if (pagination.CreatedAtBeforeOrEqual.HasValue)
+                {
+                    parameters.Append("created_at_before_or_equal", pagination.CreatedAtBeforeOrEqual.Value.ToString("o"));
+                }
+                if (pagination.CreatedAtAround.HasValue)
+                {
+                    parameters.Append("created_at_around", pagination.CreatedAtAround.Value.ToString("o"));
+                }
+            }
+
+            return Get<GetRepliesResponseInternalDTO>($"messages/{parentId}/replies", parameters);
+        }
     }
 }
